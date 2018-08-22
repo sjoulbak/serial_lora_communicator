c9a2cc54417aca8f1201b4683371cfbfaae047e6
sjoulbak <remco_meinen@hotmail.com>
2018-08-22 20:03:48 +0200

    It should be possible to send a command

--- lib/serial_lora_communicator/serial/uart_gen_server.ex
+++ lib/serial_lora_communicator/serial/uart_gen_server.ex
@@ -2,7 +2,7 @@ defmodule SerialLoraCommunicator.Serial.UartGenServer do
   use GenServer
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+    GenServer.start_link(__MODULE__, %{}, [name: __MODULE__])
   end
 
   def init(_) do
@@ -10,23 +10,32 @@ defmodule SerialLoraCommunicator.Serial.UartGenServer do
 
     state = %{
       pid: pid,
-      disconnected_names: ["ttyUSB0"]
+      disconnected_names: ["ttyUSB0"],
+      failed_commands: []
     }
 
     {:ok, state, 1000}
   end
 
+  def handle_cast({:send_command, command}, state) do
+    failed_commands = case send_command(command, state) do
+      :ok -> state.failed_commands
+      _ -> state.failed_commands ++ [command]
+    end
 
-  def handle_info({:nerves_uart, name, {:error, :eio}}, state) do
+    {:noreply, Map.merge(state, %{failed_commands: failed_commands}), 1000}
+  end
+
+  def handle_info({:nerves_uart, "ttyUSB0" = name, {:error, :eio}}, state) do
     {:noreply, Map.merge(state, %{disconnected_names: state.disconnected_names ++ [name]}), 1000}
   end
-  def handle_info({:nerves_uart, name, text}, state) do
+  def handle_info({:nerves_uart, "ttyUSB0", text}, state) do
     IO.inspect("----[*]----")
-    IO.inspect(name)
     IO.inspect(text)
 
     {:noreply, state}
   end
+  def handle_info({:nerves_uart, _, _}, state), do: {:noreply, state}
 
   def handle_info(:timeout, %{disconnected_names: disconnected_names} = state) when length(disconnected_names) > 0 do
     disconnected_names = Enum.reject(disconnected_names, fn(name) ->
@@ -36,12 +45,20 @@ defmodule SerialLoraCommunicator.Serial.UartGenServer do
       end
     end)
 
-    state = Map.merge(state, %{disconnected_names: disconnected_names})
-    if length(disconnected_names) > 0 do
-      {:noreply, state, 1000}
-    else
-      {:noreply, state}
-    end
+    {:noreply, Map.merge(state, %{disconnected_names: disconnected_names}), 1000}
+  end
+  def handle_info(:timeout, %{failed_commands: failed_commands} = state) when length(failed_commands) > 0 do
+    failed_commands = Enum.reject(failed_commands, fn(command) ->
+      case send_command(command, state) do
+        :ok -> true
+        _ -> false
+      end
+    end)
+
+    {:noreply, Map.merge(state, %{failed_commands: failed_commands}), 1000}
   end
   def handle_info(:timeout, state), do: {:noreply, state}
+
+  defp send_command(_, %{disconnected_names: disconnected_names}) when length(disconnected_names) > 0, do: :error
+  defp send_command(command, %{pid: pid}), do: Nerves.UART.write(pid, command)
 end
